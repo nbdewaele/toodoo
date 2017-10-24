735574fd335e7a7af52c41e868324f046b9a53df
Nate DeWaele <nbdewaele@gmail.com>
2017-10-24 12:53:19 -0400

    ajaxified and jquery gem added

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -29,7 +29,8 @@ class TasksController < ApplicationController
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to @task, notice: 'Task was successfully created.' }
+        # format.html { redirect_to @task, notice: 'Task was successfully created.' }
+				format.html { redirect_to tasks_url }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new }
