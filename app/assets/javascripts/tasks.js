8ef94d40539b8fd2207183435e277e9c6d21a725
Nate DeWaele <nbdewaele@gmail.com>
2017-10-24 13:51:58 -0400

    edit page ajaxified - bootstrap styles added

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -1,2 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+  console.log('document is ready!', new Date());
+	$('#task_name').focus();
+
+	$('.modal-footer .btn-primary').click(function() {
+    console.log('save the changes!');
+  });
+});
